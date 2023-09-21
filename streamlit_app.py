c09aeae57fa64b936846dec965223ed0e269717a
danielmartinr449 <68743443+danielmartinr449@users.noreply.github.com>
2023-09-21 11:12:48 -0400

    Update streamlit_app.py
    Adding fruit list button and function to display the fruit list on push button

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,17 +43,28 @@ except URLError as e:
  #fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - Publish the normalized data into the application
  #streamlit.dataframe(fruityvice_normalized)
-streamlit.stop()
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
+#my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+#my_cur = my_cnx.cursor()
 #my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_cur.execute("SELECT * from fruit_load_list")
-my_data_row = my_cur.fetchall()
+#my_cur.execute("SELECT * from fruit_load_list")
+#my_data_rows = my_cur.fetchall()
 #streamlit.text("Hello from Snowflake:")
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_row)
+#streamlit.header("The fruit load list contains:")
+def get_fruit_load_list():
+  with my_cnx.cursor as my_cur:
+    my_cur.execute("Select * from fruit_load_list")
+    return my_cur.fetchall()
 
+#Add a button to get load list
+if streamlit.button('Get Fruit List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows=get_fruit_load_list()
+  streamlit.header("The fruit load list contains:")
+  streamlit.dataframe(my_data_rows)
+  
+#streamlit.dataframe(my_data_rows)
+streamlit.stop()
 #add user fruit
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 streamlit.write('Thanks for adding ',add_my_fruit)
