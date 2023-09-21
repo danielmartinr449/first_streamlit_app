0580b033af6838e193be6fef62d336a898ef608c
danielmartinr449 <68743443+danielmartinr449@users.noreply.github.com>
2023-09-21 11:34:57 -0400

    Update streamlit_app.py
    Added connection close and included a title for getting fruit list and adding one

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,10 +57,12 @@ def get_fruit_load_list():
     return my_cur.fetchall()
 
 #Add a button to get load list
+streamlit.title("View our Fruit List - Add your Favorites!")
 if streamlit.button('Get Fruit List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_rows=get_fruit_load_list()
-  streamlit.header("The fruit load list contains:")
+  my_cnx.close()
+  streamlit.header("The fruit list contains:")
   streamlit.dataframe(my_data_rows)
   
 #streamlit.dataframe(my_data_rows)
@@ -71,11 +73,12 @@ if streamlit.button('Get Fruit List'):
 
 def insert_row_snowflake(new_fruit):
   with my_cnx.cursor() as my_cur:
-    my_cur.execute("insert into fruit_load_list values('from streamlit')")
+    my_cur.execute("insert into fruit_load_list values('"+new_fruit+"')")
     return "Thanks for adding "+new_fruit
 
 add_my_fruit=streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add a Fruit to the List'):
   my_cnx=snowflake.connector.connect(**streamlit.secrets["snowflake"])
   back_from_function=insert_row_snowflake(add_my_fruit)
+  my_cnx.close()
   streamlit.text(back_from_function)
