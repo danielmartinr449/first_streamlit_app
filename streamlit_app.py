6345117335633a5835994a9679102661785c1fe7
danielmartinr449 <68743443+danielmartinr449@users.noreply.github.com>
2023-09-21 11:14:26 -0400

    Update streamlit_app.py
    typo mistake on mycursor

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,7 +52,7 @@ except URLError as e:
 #streamlit.text("Hello from Snowflake:")
 #streamlit.header("The fruit load list contains:")
 def get_fruit_load_list():
-  with my_cnx.cursor as my_cur:
+  with my_cnx.cursor() as my_cur:
     my_cur.execute("Select * from fruit_load_list")
     return my_cur.fetchall()
 
